d55dac6fda913c3dac7097e5ee7069f52e177a5a
Pius Erheyovwe BUBU <34315856+bubulizer@users.noreply.github.com>
2026-07-29 04:45:38 +0300

    Connect PhD and Master's Google Forms to audit page

--- docs/config.js
+++ docs/config.js
@@ -1,4 +1,4 @@
 window.KIU_AUDIT_FORMS = {
-  phd: "",
-  masters: ""
+  phd: "https://docs.google.com/forms/d/e/1FAIpQLSfyiQEQ7ZdJutWwrg9OcuMTn-5cyWo9Rs1zGxXKSlGw5kUu2w/viewform",
+  masters: "https://docs.google.com/forms/d/e/1FAIpQLSfj-Qt-jAKaXMADxlyeH4VWxIF9H5_8mxIsqyJxkkvtjFIDIA/viewform"
 };
