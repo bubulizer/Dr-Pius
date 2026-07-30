08710a4add14e940952a83fe233472d1736770ce
Pius Erheyovwe BUBU <34315856+bubulizer@users.noreply.github.com>
2026-07-30 16:09:20 +0300

    Point site to Associate Dean-owned audit form

--- docs/config.js
+++ docs/config.js
@@ -1,4 +1,4 @@
-window.KIU_AUDIT_FORMS = {
-  phd: "https://docs.google.com/forms/d/e/1FAIpQLSfyiQEQ7ZdJutWwrg9OcuMTn-5cyWo9Rs1zGxXKSlGw5kUu2w/viewform",
-  masters: "https://docs.google.com/forms/d/e/1FAIpQLSfj-Qt-jAKaXMADxlyeH4VWxIF9H5_8mxIsqyJxkkvtjFIDIA/viewform"
+window.KIU_SEAS_AUDIT = {
+  studentForm: "https://docs.google.com/forms/d/e/1FAIpQLSeAc_ylO9DZg8YZldAxGuOKJdNjYCVtpCjRQD2qRIjju5LQjQ/viewform",
+  deadline: "To be communicated by the Research Coordinator"
 };
